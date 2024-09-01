dbe1b40d3a13bcc67ff45ffc552ec50712217dfd
Amos Wong <amoswwj@gmail.com>
2024-09-01 17:02:31 +0800

    fix: remove basedir in resolveOptions, allow original NodeJS resolve algorithm
    close #233

--- src/index.js
+++ src/index.js
@@ -203,7 +203,6 @@ async function bundleMDX({
         // eslint-disable-next-line new-cap
         NodeResolvePlugin({
           extensions: ['.js', '.ts', '.jsx', '.tsx'],
-          resolveOptions: {basedir: cwd},
         }),
         inMemoryPlugin,
         mdxESBuild(
