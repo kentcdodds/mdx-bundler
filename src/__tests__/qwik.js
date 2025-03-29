8f1a866347f9d21e04ca23aef6048e6848ef3ada
thejackshelton <me@jackshelton.com>
2025-03-28 22:45:38 -0500

    initial qwik test

---
+++ src/__tests__/qwik.js
@@ -0,0 +1,83 @@
+/** @jsxImportSource @builder.io/qwik */
+
+import './setup-tests.js'
+import * as Qwik from "@builder.io/qwik";
+import { jsx as _jsx_runtime } from '@builder.io/qwik';
+import {suite} from 'uvu'
+import * as assert from 'uvu/assert'
+import { render } from '@noma.to/qwik-testing-library'
+import {bundleMDX} from '../index.js'
+import {getMDXComponent} from '../client/jsx.js'
+
+const test = suite("qwik");
+
+const jsxBundlerConfig = {
+  jsxLib: {
+    varName: 'Qwik',
+    package: '@builder.io/qwik',
+  },
+  jsxRuntime: {
+    varName: '_jsx_runtime',
+    package: '@builder.io/qwik',
+  },
+}
+const jsxComponentConfig = { Qwik, _jsx_runtime }
+
+const mdxSource = `
+---
+title: Example Post
+published: 2021-02-13
+description: This is some meta-data
+---
+import Demo from './demo'
+
+# This is the title
+
+Here's a **neat** demo:
+<Demo />
+`.trim();
+
+const demoTsx = `
+export default function Demo() {
+  return <div>mdx-bundler with Qwik's runtime!</div>
+}
+`.trim();
+
+test('smoke test for qwik', async () => {
+  const result = await bundleMDX({
+    source: mdxSource,
+    jsxConfig: jsxBundlerConfig,
+    files: {
+      './demo.tsx': demoTsx
+    }
+  });
+
+  /** 
+   * @type {import('@builder.io/qwik').Component<{ components?: Record<string, any> }>}
+   */
+  const Component = getMDXComponent(result.code, jsxComponentConfig)
+
+  /** @type {Qwik.Component<{}>} */
+  const SpanBold = Qwik.component$(() => {
+    return <span class="strong">neat</span>
+  })
+
+  assert.equal(result.frontmatter, {
+    title: 'Example Post',
+    published: new Date('2021-02-13'),
+    description: 'This is some meta-data',
+  })
+
+  const {container} = render(
+    <Component components={{strong: SpanBold}} />
+  )
+
+  assert.equal(
+    container.innerHTML,
+    `<h1>This is the title</h1>
+<p>Here's a <span class="strong">neat</span> demo:</p>
+<div>mdx-bundler with Qwik's runtime!</div>`,
+  )
+})
+
+test.run()
